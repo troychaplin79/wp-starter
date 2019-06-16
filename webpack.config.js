a1751246c5559d4954b2b075650bfa366a269b0d
Troy Chaplin <troy.chaplin@gmail.com>
2019-06-16 14:20:05 -0400

    Added browserlist

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,12 @@ module.exports = {
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				loader: 'babel-loader'
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
 			},
 			{
 				test: /\.scss$/,
