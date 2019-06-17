e1ba8ea2b0dffa53b672161661ceb980cb91eeb3
Troy Chaplin <troy.chaplin@gmail.com>
2019-06-16 20:18:48 -0400

    Minor updates

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,15 +1,11 @@
 module.exports = {
 	plugins: [
-		// TODO: why doesn't this work inside cssnano preset
 		require('autoprefixer')({
 			flexbox: false,
 			grid: false,
 		}),
 		require('css-mqpacker')({
 			sort: true,
-		}),
-		require('postcss-pxtorem')({
-			replace: true,
 		}),
 		require('cssnano')({
 			preset: ['advanced', {
@@ -21,6 +17,9 @@ module.exports = {
 				},
 			}]
 			// zindex: false,
+		}),
+		require('postcss-pxtorem')({
+			replace: true,
 		}),
 	],
 };
