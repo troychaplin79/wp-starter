1240cfa8920a32e8ae7f12e04da8a99e6cd8d45e
Troy Chaplin <troy.chaplin@gmail.com>
2019-07-11 08:34:20 -0400

    Cleaned up and updated global functions

--- functions/global/acf-gutenberg.php
+++ functions/global/acf-gutenberg.php
@@ -1,15 +1,12 @@
 <?php
 /**
  * Add custom block categories
- * =============
- *
- * @package Theme Functions
+
+ * @package Theme Global Supports
  * @category ACF + Gutenberg
  * @version 1.0
- * @see https://loomo.ca/gutenberg-creating-custom-block-categories/
  */
-
-function pse_block_category( $categories, $post ) {
+function acf_blocks_block_category( $categories, $post ) {
 	return array_merge(
 		$categories,
 		array(
@@ -20,20 +17,18 @@ function pse_block_category( $categories, $post ) {
 		)
 	);
 }
-add_filter( 'block_categories', 'pse_block_category', 10, 2 );
+add_filter( 'block_categories', 'acf_blocks_block_category', 10, 2 );
 
 
 /**
  * Disable all default gutenberg blocks
- * =============
  *
- * @package Shared Libraries
- * @category ACF Gutenberg
+ * @package Theme Functions
+ * @category ACF + Gutenberg
  * @version 1.0
  * @see other uses for this function: https://rudrastyh.com/gutenberg/remove-default-blocks.html
  * @see a list of core blocks: https://wpdevelopment.courses/a-list-of-all-default-gutenberg-blocks-in-wordpress-5-0/
  */
-
 function pseweb_allowed_block_types( $allowed_blocks, $post ) {
 
 	// add all acf/blocks here, restrict to post types in acf_register_block functions
@@ -48,15 +43,12 @@ add_filter( 'allowed_block_types', 'pseweb_allowed_block_types', 10, 2 );
 
 /**
  * Register the Blocks
- * =============
  *
- * @package Theme Includes
+ * @package Theme Functions
  * @category ACF + Gutenberg
  * @version 1.0
- * @author troychaplin
  * @see https://www.advancedcustomfields.com/resources/acf_register_block/
  */
-
 function pseweb_register_acf_blocks() {
 
 	// check function exists
@@ -83,15 +75,11 @@ add_action( 'acf/init', 'pseweb_register_acf_blocks' );
 
 /**
  * Disable gutenberg front end styles
- * =============
  *
  * @package Theme Functions
  * @category ACF + Gutenberg
  * @version 1.0
  */
-
-// TODO: wrap in acf option
-
 function pseweb_deregister_styles() {
 	wp_dequeue_style( 'wp-block-library' );
 }
