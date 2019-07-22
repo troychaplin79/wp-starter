81ee37ff93e4cb32d311432049df22303192952e
Troy Chaplin <troy.chaplin@gmail.com>
2019-07-22 15:25:21 -0400

    Removed code that will break without wp-homepage plugin

--- home.php
+++ home.php
@@ -1,11 +1,8 @@
 <?php
 // Set current post id
-$current_id      = get_field('active_homepage', 'wp_homepage_options');
+$current_id      = ''; // TODO: bring this back to get homepage content by ID --> get_field('active_homepage', 'wp_homepage_options');
 $get_post_type   = get_post_type($current_id);
 $clean_post_type = str_replace('wp-', '', $get_post_type); // remove post type prefix, if applicable
-
-// Get the home id & content
-$homepage_content = apply_filters('the_content', get_post_field('post_content', $current_id));
 
 require_once get_template_directory() . '/header.php';
 require_once get_template_directory() . '/blocks/banner/home.php';
