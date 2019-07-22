2c3fda0a21baabd767e223c4f6b8ea8ca5a9720c
Troy Chaplin <troy.chaplin@gmail.com>
2019-07-22 15:19:07 -0400

    Updated home.php

--- home.php
+++ home.php
@@ -1,18 +1,18 @@
 <?php
 // Set current post id
-$current_id      = ''; // TODO: update with ID for homepage based on homepage post type option --> get_field( 'active_homepage', 'pse_homepage_options' );
-$get_post_type   = get_post_type( $current_id );
-$clean_post_type = str_replace( 'cpt-', '', $get_post_type ); // remove post type prefix, if applicable
+$current_id      = get_field('active_homepage', 'wp_homepage_options');
+$get_post_type   = get_post_type($current_id);
+$clean_post_type = str_replace('wp-', '', $get_post_type); // remove post type prefix, if applicable
 
 // Get the home id & content
-$homepage_content = apply_filters( 'the_content', get_post_field( 'post_content', $current_id ) );
+$homepage_content = apply_filters('the_content', get_post_field('post_content', $current_id));
 
 require_once get_template_directory() . '/header.php';
-// require_once get_template_directory() . '/blocks/banner/home.php';
+require_once get_template_directory() . '/blocks/banner/home.php';
 
 echo '<main>';
+echo get_post_field( 'post_content', $current_id ); // @codingStandardsIgnoreLine
 require_once get_template_directory() . '/templates/_examples/grid.php'; // TODO: remove, example only
-echo $homepage_content; // @codingStandardsIgnoreLine
 echo '</main>';
 
 require_once get_template_directory() . '/footer.php';
