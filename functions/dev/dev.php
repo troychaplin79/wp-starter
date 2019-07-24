6ffc6690ed45a6ba08ae0c610f7e56d8ee00c92c
Troy Chaplin <troy.chaplin@gmail.com>
2019-07-24 12:10:43 -0400

    Updated dev function for theme / page info

--- functions/dev/dev.php
+++ functions/dev/dev.php
@@ -5,33 +5,37 @@ function get_page_info()
     global $current_id, $get_post_type, $template_name, $current_tax_name, $current_term_slug;
 
     $output = '<div class="u-block u-block--white"><div>';
-    $output .= '<h2>Page Information</h2>';
-    $output .= '<ul>';
 
-    
+    // Theme meta
+    $get_current_theme     = wp_get_theme();
+    $current_theme_name    = $get_current_theme->get('Name');
+    $current_theme_version = $get_current_theme->get('Version');
+    $current_theme_dir  = $get_current_theme->template;
+
+    $output .= '<h3>Theme Meta</h3>';
+    $output .= '<ul>';
+    $output .= '<li>Active theme: ' . $current_theme_name . '</li>';
+    $output .= '<li>Theme version: ' . $current_theme_version . '</li>';
+    $output .= '</ul>';
+
+    $output .= '<h3>Page Information</h3>';
+    $output .= '<ul>';
 
     // home / page
     if (is_home() || is_page()) {
         $output .= '<li>Current ID: ' . $current_id . '</li>';
-        $output .= '<li>Active Post Type: ' . $get_post_type . '</li>';
-        $output .= '<li>Core File Loaded: ' . dirname(__FILE__) . '/home.php</li>';
+        $output .= '<li>Active post type: ' . $get_post_type . '</li>';
+        $output .= '<li>Core file loaded: ' . $current_theme_dir . '/home.php</li>';
     }
 
     // tax
     if (is_category() || is_tax()) {
         $output .= '<li>Term ID: ' . $current_id . '</li>';
-        $output .= '<li>Core File Loaded: ' . dirname(__FILE__) . '/taxonomy.php</li>';
-
-        $output .= '<br>';
-
-        $output .= '<li>Associated Post Type: ' . $get_post_type . '</li>';
-
-        $output .= '<br>';
-
-        $output .= '<li>Current taxonomy slug is ' . $current_tax_name . '</li>';
-        $output .= '<li>Current term slug is ' . $current_term_slug . '</li>';
-        $output .= '<li>Current post type being loaded is ' . $get_post_type . '</li>';
-        $output .= '<li>Additional file being included: ' . dirname(__FILE__) . '/' . $template_name . '.php</li>';
+        $output .= '<li>Core file loaded: ' . $current_theme_dir . '/taxonomy.php</li>';
+        $output .= '<li>Current taxonomy slug: ' . $current_tax_name . '</li>';
+        $output .= '<li>Current term slug: ' . $current_term_slug . '</li>';
+        $output .= '<li>Associated post type: ' . $get_post_type . '</li>';
+        $output .= '<li>Template include: ' . $current_theme_dir . '/templates/taxonomy/' . $template_name . '.php</li>';
     }
 
     $output .= '</ul>';
@@ -39,4 +43,3 @@ function get_page_info()
 
     echo $output;
 }
-// add_action('wp_head', 'get_page_info');
