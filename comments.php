dd0b249d4cde6252e8f49171a8f797125b28a4a3
Troy Chaplin <troy.chaplin@gmail.com>
2019-07-24 13:49:47 -0400

    Added basic templating structure

---
+++ comments.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Single post comment template
+ * =============
+ *
+ * @package Theme Templates
+ * @category Core Templating
+ * @version 1.0
+ * @see https://www.gavsblog.com/blog/adding-comments-to-a-wordpress-theme-template
+ */
+
+if (have_comments()) :
+    echo '<ol class="c-comments">';
+        wp_list_comments(
+            array(
+                'style'      => 'ol',
+                'short_ping' => true,
+            )
+        );
+    echo '</ol>';
+endif;
--- templates/single/post.php
+++ templates/single/post.php
@@ -1,4 +1,10 @@
 <?php
 echo '<main>';
     the_content();
+
+if (comments_open()) {
+    comments_template();
+    comment_form();
+}
+
 echo '</main>';
