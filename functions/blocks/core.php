7484c9dc043946d6ad65a1c974acf3884ed89091
Troy Chaplin <troy.chaplin@gmail.com>
2019-08-20 23:31:32 -0400

    Updated allowed blocks function to separate block types, show blocks only for admins, and added theme supports to remove block options

--- functions/blocks/core.php
+++ functions/blocks/core.php
@@ -13,7 +13,7 @@ function dequeue_gutenberg_library()
 add_action('wp_print_styles', 'dequeue_gutenberg_library', 100);
 
 /**
- * Disable all default gutenberg blocks
+ * Specify allowed core blocks
  *
  * @package Theme ACF Supports
  * @category ACF + Gutenberg
@@ -21,39 +21,44 @@ add_action('wp_print_styles', 'dequeue_gutenberg_library', 100);
  * @see other uses for this function: https://rudrastyh.com/gutenberg/remove-default-blocks.html
  * @see a list of core blocks: https://wpdevelopment.courses/a-list-of-all-default-gutenberg-blocks-in-wordpress-5-0/
  */
-function acf_blocks_allowed_types($allowed_blocks, $post)
+function set_allowed_core_blocks($final_blocks, $post)
 {
-    // add all custom acf blocks here, restrict to post types in register_custom_acf_blocks functions
-    $allowed_blocks = array(
-
-        // common
+    // Set available core blocks
+    $core_blocks = array(
         'core/paragraph',
         'core/image',
         'core/heading',
         'core/list',
         'core/quote',
-        // 'core/file', // TODO: consider to future implementation
         'core/video',
-
-        // formatting
-        // 'core/table', // TODO: create custom version or modify output
-        'core/code', // TODO: consider to future implementation
-        'core/html', // TODO: make visible only to admins
-
-        // layout
-        // 'core/button', // TODO: create custom version or modify output
-        // 'core/columns', // TODO: create custom version or modify output
-        // 'core/media-text', // TODO: create custom version or modify output
-
-        // widgets
-        'core/shortcode', // TODO: make visible only to admins for migration, removd once upgrade is complete
-
+        // 'core/table', // TODO: create custom render_block function
+        // 'core/code', // TODO: create custom render_block function
+        // 'core/button', // TODO: create custom render_block function
+        // 'core/columns', // TODO: create custom render_block function
+        // 'core/media-text', // TODO: create custom render_block function
+    );
+    
+    // Register custom blocks
+    $custom_blocks = array();
+    
+    // Display specific blocks only to admins
+    $admin_blocks = array();
+    
+    if (current_user_can('administrator')) {
+        $admin_blocks = array(
+            'core/html',
+            'core/shortcode',
+        );
+    }
+    
+    $plugin_blocks = array(
         'gravityforms/form',
     );
 
-    return $allowed_blocks;
+    $final_blocks = array_merge($core_blocks, $custom_blocks, $admin_blocks, $plugin_blocks);
+    return $final_blocks;
 }
-add_filter('allowed_block_types', 'acf_blocks_allowed_types', 10, 2);
+add_filter('allowed_block_types', 'set_allowed_core_blocks', 10, 2);
 
 /**
  * Modify Heading Block Rendering
@@ -77,3 +82,16 @@ function modify_heading_block($block_content, $block)
     return $return;
 }
 add_filter('render_block', 'modify_heading_block', 10, 2);
+
+/**
+ * Remove / Customize Block Inspector Elements
+ *
+ * @package Block Options
+ * @category Gutenberg Supports
+ * @version 1.0
+ * @see https://developer.wordpress.org/block-editor/developers/themes/theme-support/#block-font-sizes
+ */
+add_theme_support('disable-custom-font-sizes'); // Disable manually entered font sizes
+add_theme_support('editor-font-sizes'); // Remove preset font sizes
+add_theme_support('disable-custom-colors'); // Disable custom color options
+add_theme_support('editor-color-palette'); // Remove color picker
