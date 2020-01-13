2a0e6b41858ad424c2e8b3c2c9b2dc8679917545
Troy Chaplin <troy.chaplin@gmail.com>
2020-01-12 22:50:45 -0500

    Added prettier and babelrc dot files, updated spacing on other dot files

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,28 +1,28 @@
 module.exports = {
-	plugins: [
-		require("autoprefixer")({
-			flexbox: false,
-			grid: false
-		}),
-		require("css-mqpacker")({
-			sort: true
-		}),
-		require("cssnano")({
-			preset: [
-				"advanced",
-				{
-					cssDeclarationSorter: {
-						order: "concentric-css"
-					},
-					discardComments: {
-						removeAll: true
-					}
-				}
-			]
-			// zindex: false,
-		}),
-		require("postcss-pxtorem")({
-			replace: true
-		})
-	]
+    plugins: [
+        require("autoprefixer")({
+            flexbox: false,
+            grid: false,
+        }),
+        require("css-mqpacker")({
+            sort: true,
+        }),
+        require("cssnano")({
+            preset: [
+                "advanced",
+                {
+                    cssDeclarationSorter: {
+                        order: "concentric-css",
+                    },
+                    discardComments: {
+                        removeAll: true,
+                    },
+                },
+            ],
+            // zindex: false,
+        }),
+        require("postcss-pxtorem")({
+            replace: true,
+        }),
+    ],
 };
