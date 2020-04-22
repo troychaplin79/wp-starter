6c0a93ba28afb2196500629848082b54d83de042
Troy Chaplin <troy.chaplin@gmail.com>
2020-04-22 08:33:00 -0400

    Testing commit after sourcetree account changes

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@ wp_footer();
 // Load page information in dev
 if ('local' === getenv('ENV_CURRENT_ENV')) {
     get_page_info();
-    // require_once get_template_directory() . '/_examples/grid.php';
+    require_once get_template_directory() . '/_examples/grid.php';
 }
 ?>
 
