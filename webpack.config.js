3911ebddf021a1bb78b361253cff9e7e1f23604c
Troy Chaplin <troy.chaplin@gmail.com>
2020-08-22 09:44:24 -0400

    Updated webpack config and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -62,6 +62,13 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.svg(\?.*)?$/, // match img.svg and img.svg?param=value
+                use: [
+                    "url-loader", // or file-loader or svg-url-loader
+                    "svg-transform-loader",
+                ],
+            },
         ],
     },
     plugins: [
